dcba6f858dd979550d955e909e5e6613b160f7b8
Andriy <andriymattt@gmail.com>
2019-11-29 00:41:34 +0200

    pattern Command: add Controller interface

---
+++ src/main/java/com/epam/pattern_command/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.epam.pattern_command.controller;
+
+public interface Controller {
+
+    void lambdaCommand(String arg);
+
+    void methodReferenceCommand(String arg);
+
+    void anonymousClassCommand(String arg);
+
+    void objectCommand(String arg);
+    
+}

7d3e4d9e4fa6ae375f1cef4acc327b93de687863
Andriy <andriymattt@gmail.com>
2019-11-28 19:35:26 +0200

    add Main class with lambda functions as variables

---
+++ src/main/java/com/epam/functional_interface/Main.java
@@ -0,0 +1,26 @@
+package com.epam.functional_interface;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyFuncInterface maxLambda = (a, b, c) -> {
+            if ((a > b) && (a > c)) {
+                return a;
+            } else if ((b > a) && (b > c)) {
+                return b;
+            } else {
+                return c;
+            }
+        };
+
+        MyFuncInterface averageLambda = (a, b, c) -> {
+            return (a + b + c) / 3;
+        };
+
+        System.out.println(maxLambda.method(10, 20, 30));
+        System.out.println(maxLambda.method(30, 100, 20));
+        System.out.println(averageLambda.method(10, 20, 30));
+        System.out.println(averageLambda.method(30, 100, 20));
+    }
+}

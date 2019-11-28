d0b536a31a4de62b9f327d84867bbbfd6d932c0e
Andriy <andriymattt@gmail.com>
2019-11-29 00:45:10 +0200

    pattern Command: override methods of Controller interface

--- src/main/java/com/epam/pattern_command/controller/ControllerImpl.java
+++ src/main/java/com/epam/pattern_command/controller/ControllerImpl.java
@@ -1,23 +1,33 @@
 package com.epam.pattern_command.controller;
 
+import com.epam.pattern_command.model.BusinessLogic;
+import com.epam.pattern_command.model.Model;
+
 public class ControllerImpl implements Controller {
+
+    private Model model;
+
+    public ControllerImpl() {
+        this.model = new BusinessLogic();
+    }
+
     @Override
     public void lambdaCommand(String arg) {
-        
+        model.lambdaCommand(arg);
     }
 
     @Override
     public void methodReferenceCommand(String arg) {
-
+        model.methodReferenceCommand(arg);
     }
 
     @Override
     public void anonymousClassCommand(String arg) {
-
+        model.anonymousClassCommand(arg);
     }
 
     @Override
     public void objectCommand(String arg) {
-
+        model.objectCommand(arg);
     }
 }

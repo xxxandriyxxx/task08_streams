10d7d9281e31e8bd65f32bdff0adb3a9da5cf879
Andriy <andriymattt@gmail.com>
2019-11-29 01:06:17 +0200

    pattern Command: add Main class with entry point

---
+++ src/main/java/com/epam/pattern_command/Main.java
@@ -0,0 +1,10 @@
+package com.epam.pattern_command;
+
+import com.epam.pattern_command.View.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new View().show();
+    }
+}

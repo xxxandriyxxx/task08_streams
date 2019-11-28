9c5de2eec5364d4186fd2d43148d2795fce70831
Andriy <andriymattt@gmail.com>
2019-11-28 23:17:51 +0200

    pattern Command: add constructor to Print class

--- src/main/java/com/epam/pattern_command/model/Print.java
+++ src/main/java/com/epam/pattern_command/model/Print.java
@@ -4,9 +4,13 @@ public class Print implements Command {
 
     private Receiver receiver;
 
+    public Print(Receiver receiver) {
+        this.receiver = receiver;
+    }
+
     @Override
     public void execute(String arg) {
         receiver.Print(arg);
     }
-    
+
 }

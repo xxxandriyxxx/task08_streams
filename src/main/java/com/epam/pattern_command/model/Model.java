4223c073b0b6ada12dba04bf054511fbea4ecc83
Andriy <andriymattt@gmail.com>
2019-11-28 22:39:12 +0200

    pattern Command: add Model interface with methods

---
+++ src/main/java/com/epam/pattern_command/model/Model.java
@@ -0,0 +1,13 @@
+package com.epam.pattern_command.model;
+
+public interface Model {
+
+    void lambdaCommand(String arg);
+
+    void methodReferenceCommand(String arg);
+
+    void anonymousClassCommand(String arg);
+
+    void objectCommand(String arg);
+
+}

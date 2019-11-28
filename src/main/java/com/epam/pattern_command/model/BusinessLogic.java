ada7dc862747bddb65e82fcbff8b08c4cbc5575d
Andriy <andriymattt@gmail.com>
2019-11-29 00:37:02 +0200

    pattern Command: add BusinessLogic class that implements Model interface, override methods

---
+++ src/main/java/com/epam/pattern_command/model/BusinessLogic.java
@@ -0,0 +1,36 @@
+package com.epam.pattern_command.model;
+
+public class BusinessLogic implements Model {
+
+    private Receiver receiver = new Receiver();
+    private Command receiverCommand = new ReceiverCommand(receiver);
+    private Invoker invoker = new Invoker(receiverCommand);
+
+    @Override
+    public void lambdaCommand(String arg) {
+        Command command = msg -> invoker.print("Used a lambda function || message: " + msg);
+        command.execute(arg);
+    }
+
+    @Override
+    public void methodReferenceCommand(String arg) {
+        Command command = invoker::print;
+        command.execute("Used a method reference || message: " + arg);
+    }
+
+    @Override
+    public void anonymousClassCommand(String arg) {
+        Command command = new Command() {
+            @Override
+            public void execute(String arg) {
+                invoker.print("Used an anonymous class || message: " + arg);
+            }
+        };
+        command.execute(arg);
+    }
+
+    @Override
+    public void objectCommand(String arg) {
+        invoker.print("Used an object of command class || message: " + arg);
+    }
+}

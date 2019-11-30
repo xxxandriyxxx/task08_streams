199ceb1809154b787fd233b628e9d99aba897655
Andriy <andriymattt@gmail.com>
2019-11-30 17:14:23 +0200

    stream: add methods for getting a list of random integers using a stream

--- src/main/java/com/epam/stream/RandomIntList.java
+++ src/main/java/com/epam/stream/RandomIntList.java
@@ -1,5 +1,24 @@
 package com.epam.stream;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RandomIntList {
+
+    public static List<Integer> getList1(int size, int min, int max) {
+        List<Integer> list = new ArrayList<>();
+        Stream.generate(() -> (int) (min + (Math.random() * (max - min + 1))))
+                .limit(size)
+                .forEach(list::add);
+        return list;
+    }
+
+    public static List<Integer> getList2(int size, int min, int max) {
+        return new Random().ints(min, max)
+                .limit(size)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 
 }

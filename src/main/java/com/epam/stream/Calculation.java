bc9bc9de70d96b3a79840e05fc585713281c06b2
Andriy <andriymattt@gmail.com>
2019-11-30 19:29:09 +0200

    stream: add methods of getting min and max values of list

--- src/main/java/com/epam/stream/Calculation.java
+++ src/main/java/com/epam/stream/Calculation.java
@@ -1,7 +1,18 @@
 package com.epam.stream;
 
+import java.util.List;
 
 public class Calculation {
 
+    public static int getMin(List<Integer> list) {
+//        return Collections.min(list);
+        return list.stream().min(Integer::compare).get();
+    }
 
+    public static int getMax(List<Integer> list) {
+//        return Collections.max(list);
+        return list.stream().max(Integer::compare).get();
+    }
+
+    
 }

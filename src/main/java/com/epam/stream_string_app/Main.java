2ba26efbbd1d06bf49ab4902c3297cc46ab14550
Andriy <andriymattt@gmail.com>
2019-11-30 23:19:24 +0200

    feature/stream: add Main class with entry point

---
+++ src/main/java/com/epam/stream_string_app/Main.java
@@ -0,0 +1,10 @@
+package com.epam.stream_string_app;
+
+import com.epam.stream_string_app.view.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new View().show();
+    }
+}
